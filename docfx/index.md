fb14aeb37ca77bbb6f9642233660c09e6b087b2f
simon-techkid <simon.techkid@outlook.com>
2024-11-30 17:19:07 -0800

    v1.0.0

---
+++ docfx/index.md
@@ -0,0 +1,11 @@
+---
+_layout: landing
+---
+
+# Techkid.FileHandler.Output.Formats.Xml
+
+A C# library for universal file I/O.
+
+View & Download this project on [NuGet](https://www.nuget.org/packages/Techkid.FileHandler.Output.Formats.Xml/).
+
+View the source code for this project at [GitHub](https://github.com/simon-techkid/Techkid.FileHandler.Output.Formats.Xml/).
---
+++ src/XmlSaveable.cs
@@ -0,0 +1,38 @@
+﻿// Techkid.FileHandler by Simon Field
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Techkid.FileHandler.Output.Formats.Xml;
+
+/// <summary>
+/// Provides instructions for serializing and transforming XML data.
+/// </summary>
+public abstract class XmlSaveable<TRecord>(Func<TRecord> doc, string? trackName) :
+    SaveableAndTransformableBase<TRecord, XDocument, IEnumerable<XElement>>(doc, trackName)
+{
+    protected override Encoding OutputEncoding => XmlSettings.Encoding;
+
+    /// <summary>
+    /// The XML namespace for the document type.
+    /// </summary>
+    protected virtual XNamespace Namespace => XNamespace.Xmlns;
+
+    protected override byte[] ConvertToBytes()
+    {
+        string xmlString = TransformToXmlToString();
+        return OutputEncoding.GetBytes(xmlString);
+    }
+
+    protected override XDocument TransformToXml()
+    {
+        return Document;
+    }
+
+    protected override XDocument ClearDocument()
+    {
+        return new XDocument();
+    }
+}
